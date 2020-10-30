0da91fa11c600961882d3e8f7a388dda3cbda2c7
Joseph Deborah Oluwabunmi <josephdeborah506@gmail.com>
2020-10-30 06:33:32 +0100

    first commit

---
+++ app/src/main/java/com/example/lpihubapp/MembersList.kt
@@ -0,0 +1,11 @@
+package com.example.lpihubapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MembersList : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_members_list)
+    }
+}
---
+++ app/src/main/java/com/example/lpihubapp/RegisteredMembers.kt
@@ -0,0 +1,11 @@
+package com.example.lpihubapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class RegisteredMembers : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registered_members)
+    }
+}
